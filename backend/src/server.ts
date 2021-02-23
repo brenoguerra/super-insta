649d4550f06bba337945e62db20bda3553e988cc
brenoguerra <brenoguerradev@gmail.com>
2021-02-23 02:01:39 -0300

    feat: added cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import routes from './routes';
+import cors from 'cors';
 
 import './database';
 
 const app = express();
 
+app.disable('x-powered-by');
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
